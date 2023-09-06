621c0510a1bdd81b401f3cd4146ba4018ff4dcca
Francisco Payés <payesfrancisco@gmail.com>
2023-09-05 22:26:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<template>
+  <header>
+    <div class="header__container">
+      <span>Rule of thumb.</span>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+
+</style>
