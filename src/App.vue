dcd32a6b6ca712ecd0fc524d53d671ba30a4d674
Francisco Payés <payesfrancisco@gmail.com>
2023-09-06 01:16:29 -0400

    feat: add footer links

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 
@@ -13,9 +15,31 @@ export default {
 <template>
   <div>
     <Header />
+    <div class="content">
+      <hr class="separator" />
+    </div>
+    <div class="content">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.content {
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+}
 
+.separator {
+  border-top: 2px dashed rgba(70, 70, 70, 1);
+  margin: 0 auto;
+  max-width: 68.75rem;
+}
+
+@media (min-width: 768px) {
+  .content {
+    padding-left: 1.313rem;
+    padding-right: 1.313rem;
+  }
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<template>
+  <footer class="footer__container">
+    <ul class="footer__list">
+      <li><a>Terms and Conditions</a></li>
+      <li><a>Privacy Policy</a></li>
+      <li><a>Contact Us</a></li>
+    </ul>
+  </footer>
+</template>
+
+<style lang="scss">
+.footer {
+  &__container {
+    height: 6.781rem;
+    max-width: 68.75rem;
+    margin: 0 auto;
+    margin-bottom: 2.156rem;
+    position: relative;
+  }
+
+  &__list {
+    display: flex;
+    flex-direction: column;
+    gap: 1.313rem;
+    padding-top: 1.094rem;
+  }
+
+  &__list > li > a {
+    cursor: pointer;
+  }
+}
+
+@media (min-width: 768px) {
+  .footer {
+    &__container {
+      display: flex;
+      flex-direction: column;
+      height: 4.563rem;
+      justify-content: end;
+      margin-bottom: 2.813rem;
+    }
+
+    &__list {
+      flex-direction: row;
+      gap: 1.75rem;
+      padding: 0;
+    }
+
+    &__list > li {
+      line-height: 2.25rem;
+      font-size: 1.125rem;
+    }
+  }
+}
+@media (min-width: 1440px) {
+  .footer {
+    &__list {
+      gap: 2.25rem;
+    }
+  }
+}
+</style>
