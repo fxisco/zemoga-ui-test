ca0aa1597fe81679a556c432793e74e36fbbd972
Francisco Payés <payesfrancisco@gmail.com>
2023-09-08 19:14:43 -0400

    feat(header): add nav to desktop view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,12 @@ export default {
     <div class="header__container">
       <div class="header__title-container">
         <span class="header__page-name">Rule of thumb.</span>
+        <nav class="header__nav">
+          <a>Past Trials</a>
+          <a>How It Works</a>
+          <a>Login / Sign Up</a>
+          <button aria-label="Search"><img src="../assets/search.svg" /></button>
+        </nav>
       </div>
       <button class="header__menu-icon" aria-haspopup="true" aria-expanded="false">
         <img aria-hidden="true" src="../assets/hamburger.svg" />
@@ -26,11 +32,9 @@ export default {
   z-index: 1;
 
   &__container {
-    background: linear-gradient(
-      180deg,
-      rgba(0, 0, 0, 0.698864) 0%,
-      rgba(0, 0, 0, 0.0001) 100%
-    );
+    background: linear-gradient(180deg,
+        rgba(0, 0, 0, 0.698864) 0%,
+        rgba(0, 0, 0, 0.0001) 100%);
     height: 7.5rem;
     padding-left: 0.75rem;
     padding-top: 1.875rem;
@@ -52,6 +56,10 @@ export default {
     position: absolute;
     top: 1.5rem;
     right: 1.5rem;
+  }
+
+  &__nav {
+    display: none;
   }
 }
 
@@ -77,6 +85,11 @@ export default {
       padding-top: 3.25rem;
     }
 
+    &__title-container {
+      display: flex;
+      justify-content: space-between;
+    }
+
     &__page-name {
       font-size: 2.25rem;
       line-height: 2.7rem;
@@ -84,6 +97,21 @@ export default {
 
     &__menu-icon {
       display: none;
+    }
+
+    &__nav > a {
+      cursor: pointer;
+    }
+
+    &__nav {
+      align-items: center;
+      color: #FFF;
+      display: flex;
+      font-size: 1.125rem;
+      font-weight: 300;
+      gap: 1.125rem;
+      line-height: 1.35rem;
+      top: 3.5rem;
     }
   }
 }
